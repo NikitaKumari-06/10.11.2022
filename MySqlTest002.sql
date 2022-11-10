57dd90e2c82fddf9cf27e00d8edd467d81eb9b5a
NikitaKumari-06 <112612426+NikitaKumari-06@users.noreply.github.com>
2022-11-10 14:05:12 +0530

    Add files via upload

---
+++ MySqlTest002.sql
@@ -0,0 +1,332 @@
+-- 1. Order Subtotals
+-- For each order, calculate a subtotal for each Order (identified by OrderID). This is a simple query using GROUP BY to aggregate data 
+-- for each order.
+-- ans :
+
+	SELECT OrderID,
+		SUM(UnitPrice*Quantity) AS Subtotals
+	FROM Order_details
+    GROUP BY OrderID;
+ 
+-- 2. Sales by Year
+-- This query shows how to get the year part from Shipped_Date column. A subtotal is calculated by a sub-query for each order. The
+--  sub-query forms a table and then joined with the Orders table.
+-- ans :
+
+	SELECT DISTINCT DATE(O.ShippedDate) AS ShippedDate, 
+		O.OrderID, 
+		I.Subtotal, 
+		YEAR(O.ShippedDate) AS Year
+	FROM Orders O
+	INNER JOIN(
+				SELECT DISTINCT OrderID, 
+					  SUM(UnitPrice * Quantity) AS Subtotal
+				FROM Order_details
+				GROUP BY OrderID    
+			) I
+	ON O.OrderID = I.OrderID
+	WHERE O.ShippedDate IS NOT NULL;
+
+-- 3. Employee Sales by Country
+-- For each employee, get their sales amount, broken down by country name.
+-- ans :
+
+	SELECT  E.Country,
+			E.LastName, 
+			E.FirstName, 
+			O.ShippedDate,
+			O.OrderID,
+			I.Subtotal AS Sale_Amount
+	FROM Employees E
+	INNER JOIN Orders O INNER JOIN(
+									SELECT DISTINCT OrderID, 
+										  SUM(UnitPrice * Quantity) AS Subtotal
+									FROM Order_details
+									GROUP BY OrderID    
+									) I
+	ON E.EmployeeID = O.EmployeeID
+	WHERE O.ShippedDate IS NOT NULL;	
+
+-- 4. Alphabetical List of Products
+-- This is a rather simple query to get an alphabetical list of products.
+-- ans :
+
+	SELECT  ProductID,
+			ProductName,
+            SupplierID,
+            CategoryID,
+            QuantityPerUnit,
+            UnitPrice
+	FROM Products
+    ORDER BY ProductName;
+
+-- 5. Current Product List
+-- This is another simple query. No aggregation is used for summarizing data.
+-- ans :
+
+	SELECT 	ProductID,
+			ProductName
+	FROM Products
+	WHERE Discontinued = 'n'
+	ORDER BY ProductName;
+
+-- 6. Order Details Extended
+-- This query calculates sales price for each order after discount is applied.
+-- ans :
+
+	SELECT DISTINCT O.OrderID, 
+					O.ProductID, 
+					P.ProductName, 
+					O.UnitPrice, 
+					O.Quantity, 
+					O.Discount, 
+					ROUND(O.UnitPrice * O.Quantity * (1 - O.Discount), 2) AS ExtendedPrice
+	FROM Products P
+	INNER JOIN Order_Details O ON
+    P.ProductID = O.ProductID
+	ORDER BY O.OrderID;
+
+-- 7. Sales by Category
+-- For each category, we get the list of products sold and the total sales amount. Note that, in the second query, the inner query for 
+-- table c is to get sales for each product on each order. It then joins with outer query on Product_ID. In the outer query, products are
+-- grouped for each category.
+-- ans :
+
+	SELECT DISTINCT A.CategoryID, 
+					A.CategoryName, 
+					B.ProductName, 
+					SUM(C.ExtendedPrice) AS ProductSales
+	FROM Categories A
+	INNER JOIN Products B ON A.CategoryID = B.CategoryID
+	INNER JOIN 
+	(
+		SELECT DISTINCT P.OrderID, 
+						P.ProductID, 
+						Q.ProductName, 
+						P.UnitPrice, 
+						P.Quantity, 
+						P.Discount, 
+						ROUND(P.UnitPrice * P.Quantity * (1 - P.Discount), 2) AS ExtendedPrice
+		FROM Products Q
+		INNER JOIN Order_Details P ON
+        Q.ProductID = P.ProductID
+		ORDER BY P.OrderID
+	) C ON
+    C.ProductID = B.ProductID
+	INNER JOIN Orders D ON D.OrderID = C.OrderID
+	WHERE D.OrderDate  BETWEEN DATE('1997/1/1') AND DATE('1997/12/31')
+	GROUP BY A.CategoryID,
+			A.CategoryName,
+            B.ProductName
+	ORDER BY A.CategoryName,
+			B.ProductName,
+            ProductSales;
+
+-- 8. Ten Most Expensive Products
+-- The two queries below return the same result. It demonstrates how MySQL limits the number of records returned.
+-- The first query uses correlated sub-query to get the top 10 most expensive products.
+-- The second query retrieves data from an ordered sub-query table and then the keyword LIMIT is used outside the sub-query to restrict 
+-- the number of rows returned.
+-- ans :
+
+	SELECT 
+		*
+    FROM(
+		SELECT DISTINCT ProductName AS Ten_Most_Expensive_Products, 
+						UnitPrice
+		FROM Products
+		ORDER BY UnitPrice DESC
+) AS I
+limit 10;
+
+-- 9. Products by Category
+-- This is a simple query 
+-- ans :
+
+	SELECT DISTINCT C.CategoryName, 
+					P.ProductName, 
+					P.QuantityPerUnit, 
+					P.UnitsInStock, 
+					P.Discontinued
+	FROM Categories C
+	INNER JOIN Products P ON
+    C.CategoryID = P.CategoryID
+	WHERE P.Discontinued = 'N';
+
+-- 10. Customers and Suppliers by City
+-- This query shows how to use UNION to merge Customers and Suppliers into one result set by identifying them as having different
+--  relationships to Northwind Traders - Customers and Suppliers.
+-- ans :
+
+	SELECT City, 
+			CompanyName, 
+            ContactName,
+            'Customers' AS Relationship 
+	FROM Customers
+	UNION
+	SELECT City,
+			CompanyName,
+            ContactName, 
+            'Suppliers'
+	FROM Suppliers
+	order by City, CompanyName;
+
+-- 11. Products Above Average Price
+-- This query shows how to use sub-query to get a single value (average unit price) that can be used in the outer-query.
+-- ans :
+
+	SELECT DISTINCT ProductName,
+					UnitPrice
+	FROM Products
+	WHERE UnitPrice >	(SELECT AVG(UnitPrice) 
+						FROM Products
+						)
+	ORDER BY UnitPrice;
+    
+-- 12. Product Sales for 1997
+-- This query shows how to group categories and products by quarters and shows sales amount for each quarter.
+-- ans :
+
+	SELECT DISTINCT A.CategoryName, 
+					B.ProductName, 
+					SUM(C.UnitPrice * C.Quantity * (1 - C.Discount)) AS ProductSales,
+					CONCAT('Qtr ', QUARTER(D.ShippedDate)) AS ShippedQuarter
+	FROM Categories A
+	INNER JOIN Products B ON
+    A.CategoryID = B.CategoryID
+	INNER JOIN Order_Details C ON
+    B.ProductID = C.ProductID
+	INNER JOIN Orders D ON
+    D.OrderID = C.OrderID
+    WHERE D.ShippedDate IS NOT NULL
+	GROUP BY A.CategoryName, 
+    B.ProductName, 
+    CONCAT('Qtr ', QUARTER(D.ShippedDate))
+	ORDER BY A.CategoryName, 
+			B.ProductName, 
+			ShippedQuarter;
+    
+-- 13. Category Sales for 1997
+-- This query shows sales figures by categories - mainly just aggregation with sub-query. The inner query aggregates to product level, 
+-- and the outer query further aggregates the result set from inner-query to category level.
+-- ans :
+
+SELECT  CategoryName,
+		FORMAT(SUM(ProductSales), 2) AS CategorySales
+FROM(
+		SELECT DISTINCT A.CategoryName, 
+						B.ProductName, 
+				FORMAT(SUM(C.UnitPrice * C.Quantity * (1 - C.Discount)), 2) AS ProductSales,
+				CONCAT('Qtr ', QUARTER(D.ShippedDate)) AS ShippedQuarter
+		FROM Categories AS A
+		INNER JOIN Products AS B ON A.CategoryID = B.CategoryID
+		INNER JOIN Order_Details AS C ON B.ProductID = C.ProductID
+		INNER JOIN Orders AS D ON D.OrderID = C.OrderID 
+		WHERE D.ShippedDate BETWEEN DATE('1997-01-01') AND DATE('1997-12-31')
+		GROUP BY A.CategoryName, 
+				B.ProductName, 
+				CONCAT('Qtr ', QUARTER(D.ShippedDate))
+		ORDER BY A.CategoryName, 
+				B.ProductName, 
+				ShippedQuarter
+	) AS I
+GROUP BY CategoryName
+ORDER BY CategoryName;
+
+-- 14. Quarterly Orders by Product
+-- This query shows how to convert order dates to the corresponding quarters. It also demonstrates how SUM function is used together with
+--  CASE statement to get sales for each quarter, where quarters are converted from OrderDate column.
+-- ans :
+
+	 SELECT A.ProductName, 
+			D.CompanyName, 
+		YEAR(OrderDate) AS OrderYear,
+		FORMAT(SUM(CASE QUARTER(C.OrderDate) WHEN '1' 
+			THEN B.UnitPrice*B.Quantity*(1-B.Discount) else 0 end), 0) "Qtr 1",
+		FORMAT(SUM(CASE QUARTER(C.OrderDate) WHEN '2' 
+			THEN B.UnitPrice*B.Quantity*(1-B.Discount) else 0 end), 0) "Qtr 2",
+		FORMAT(SUM(CASE QUARTER(C.OrderDate) when '3' 
+			THEN B.UnitPrice*B.Quantity*(1-B.Discount) else 0 end), 0) "Qtr 3",
+		FORMAT(SUM(CASE QUARTER(C.OrderDate) when '4' 
+			THEN B.UnitPrice*B.Quantity*(1-B.Discount) else 0 end), 0) "Qtr 4" 
+	from Products A 
+	INNER JOIN Order_Details B on A.ProductID = B.ProductID
+	INNER JOIN Orders C on C.OrderID = B.OrderID
+	INNER JOIN Customers D on D.CustomerID = C.CustomerID 
+	WHERE C.OrderDate IS NOT NULL
+	GROUP BY A.ProductName, 
+		    D.CompanyName, 
+		   year(OrderDate)
+	ORDER BY A.ProductName, D.CompanyName;
+
+-- 15. Invoice
+-- A simple query to get detailed information for each sale so that invoice can be issued.
+-- ans :
+
+	SELECT DISTINCT B.ShipName, 
+					B.ShipAddress, 
+					B.ShipCity, 
+					B.ShipRegion, 
+					B.ShipPostalCode, 
+					B.ShipCountry, 
+					B.CustomerID, 
+					C.CompanyName, 
+					C.Address, 
+					C.City, 
+					C.Region, 
+					C.PostalCode, 
+					C.Country, 
+			CONCAT(D.FirstName,  ' ', D.LastName) AS Salesperson, 
+					B.OrderID, 
+					B.OrderDate, 
+					B.RequiredDate, 
+					B.ShippedDate, 
+					A.CompanyName, 
+					E.ProductID, 
+					F.ProductName, 
+					E.UnitPrice, 
+					E.Quantity, 
+					E.Discount,
+					E.UnitPrice * E.Quantity * (1 - E.Discount) as ExtendedPrice,
+					B.Freight
+	FROM Shippers A 
+	INNER JOIN Orders B ON A.ShipperID = B.ShipVia 
+	INNER JOIN Customers C ON C.CustomerID = B.CustomerID
+	INNER JOIN Employees D ON D.EmployeeID = B.EmployeeID
+	INNER JOIN Order_Details E ON B.OrderID = E.OrderID
+	INNER JOIN Products F ON F.ProductID = E.ProductID
+	ORDER BY B.ShipName;
+ 
+-- 16. Number of units in stock by category and supplier continent
+-- This query shows that case statement is used in GROUP BY clause to list the number of units in stock for each product category and 
+-- supplier's continent. Note that, if only s.Country (not the case statement) is used in the GROUP BY, duplicated rows will exist for each product category and supplier continent.
+-- ans :
+
+	SELECT C.CategoryName AS "Product Category", 
+		   CASE WHEN S.Country IN 
+					 ('UK','Spain','Sweden','Germany','Norway',
+					  'Denmark','Netherlands','Finland','Italy','France')
+				THEN 'Europe'
+				WHEN S.Country IN ('USA','Canada','Brazil') 
+				THEN 'America'
+				ELSE 'Asia-Pacific'
+			END AS "Supplier Continent", 
+			SUM(P.UnitsInStock) AS UnitsInStock
+	FROM Suppliers S
+	INNER JOIN Products P ON P.SupplierID=S.SupplierID
+	INNER JOIN Categories C ON C.CategoryID=P.CategoryID 
+	GROUP BY C.CategoryName, 
+			 CASE WHEN S.Country IN 
+					 ('UK','Spain','Sweden','Germany','Norway',
+					  'Denmark','Netherlands','Finland','Italy','France')
+				  THEN 'Europe'
+				  WHEN S.Country IN ('USA','Canada','Brazil') 
+				  THEN 'America'
+				  ELSE 'Asia-Pacific'
+			 END
+	ORDER BY S.Country;
+	 
+
+
+
+
